bad2b958d328bf27ff80822a02eb437fce58c644
Mark <>
2021-02-23 20:44:54 -0500

    removed minion collision

--- MasterYi.lua
+++ MasterYi.lua
@@ -1,7 +1,7 @@
 
 local function Initialize_menu()
     Menu = {}
-    menu.label("Simple Thresh");
+    menu.label("Yi");
 
     menu.label("Combo")
     Menu.combo_use_q = menu.checkbox("Use Q", true)
@@ -25,7 +25,7 @@ local function Use_Q(target)
     if Local_spellbook:get_spell_slot( spell_slot_t.q ):is_ready() and globals.get_game_time() > Spell_limiter_q then
         --pred speed, range, width, cast time
         local pred_pos = target:get_predicted_position( Local_hero:get_position() , 10000, 600, 500, 0 )
-        if pred_pos:length() > 1 and not collision.is_minion_in_line( pred_pos, 130 ) then
+        if pred_pos:length() > 1 then
             input.send_spell( spell_slot_t.q , pred_pos )
             Spell_limiter_q = globals.get_game_time() + 0.5
         end
