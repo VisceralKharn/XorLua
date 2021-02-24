7f40f486e1d7a66170386200650987b3f3e5f7cb
Mark <>
2021-02-23 20:06:18 -0500

    added mixed mode - harasses enemies using q while farming with T

--- Ziggs.lua
+++ Ziggs.lua
@@ -11,6 +11,9 @@ local function Initialize_menu()
     Menu.combo_use_e = menu.checkbox("Use E", true)
     Menu.combo_mana_e = menu.slider_int( "Mana E", 0, 100, 10)
 
+    menu.label("Mixed")
+    Menu.mixed_use_q = menu.checkbox("Use Q", true)
+    Menu.mixed_mana_q = menu.slider_int( "Mana Q", 0, 100, 10)
 
     menu.label("Draw")
     Menu.draw_q = menu.checkbox("Draw Q range", true)
@@ -19,14 +22,16 @@ end
 
 local function Init()
     Spell_limiter_q, Spell_limiter_w, Spell_limiter_e, Spell_limiter_r, max_mana  = 0,0,0,0,0
-
+    qRange = 850
+    qSpeed = 1700
+    qSize = 75
     Initialize_menu()
 end
 
 local function Use_Q(target)
     if Local_spellbook:get_spell_slot( spell_slot_t.q ):is_ready() and globals.get_game_time() > Spell_limiter_q then
         --pred speed, travel range, width, cast time
-        local pred_pos = target:get_predicted_position( Local_hero:get_position() , 1700, 850, 75, 0.25 )
+        local pred_pos = target:get_predicted_position( Local_hero:get_position() , qSpeed, qRange, qSize, 0.25 )
         if pred_pos:length() > 1 then
             input.send_spell( spell_slot_t.q , pred_pos )
             Spell_limiter_q = globals.get_game_time() + 0.5
@@ -56,6 +61,23 @@ local function Use_E(target)
     end
 end
 
+local function GetEnemies()
+    enemyTable = object_manager.get_valid_enemy_heroes()
+    for i,e in ipairs(enemyTable) do 
+        render.text(vec2:new(200,200), tostring((Local_hero:get_position() - e:get_position()):length()),32,color:new( 255, 255, 255 ) ) end
+
+end
+
+local function Mixed()
+    enemyTable = object_manager.get_valid_enemy_heroes()
+    for i,e in ipairs(enemyTable) do 
+        if (Local_hero:get_position() - e:get_position()):length() <= qRange then 
+            --render.text(vec2:new(200,200), tostring((Local_hero:get_position() - e:get_position()):length()),32,color:new( 255, 255, 255 ) ) end
+            local target = e
+            if Menu.mixed_use_q:get_value() and Local_hero:get_mana() > max_mana * (Menu.mixed_mana_q:get_value()/100) and Local_hero:get_mana() > 40 then Use_Q(target) end
+        end
+    end
+end
 
 
 local function Combo()
@@ -69,19 +91,25 @@ local function Combo()
 end
 
 
+    
+
 local function Draw()
     Local_hero = object_manager.get_local()
     Local_spellbook = Local_hero:get_spell_book()
-
-    if Menu.draw_q:get_value() then render.circle_3d( Local_hero:get_position() , 1100, color:new( 0,125,255, 100 ) ) end
-
+    myPosString = tostring(Local_hero:get_position())
+    -- example usage
+    --render.text( vec2:new( 100, 100 ), "im a text", 32, color:new( 255, 255, 255 ) )
+    render.text(vec2:new( 100, 100 ), myPosString, 32, color:new( 255, 255, 255 ) )
+    if Menu.draw_q:get_value() then render.circle_3d( Local_hero:get_position() , 850, color:new( 0,125,255, 100 ) ) end
     if Local_hero:get_mana() > max_mana then max_mana = Local_hero:get_mana() return end
 end
 
 local function Tick()
     if input.is_key_down(67) then Combo() return end
+    if input.is_key_down(84) then Mixed() return end
 end
 
 Init()
 register_callback( "draw", Tick )
 register_callback( "draw", Draw )
+--register_callback( "draw", GetEnemies )
