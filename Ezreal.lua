ce6ca1e2cbfc336de05c588cf8d7d2def35ec71b
Mark <>
2021-02-25 20:45:19 -0500

    Update Ezreal.lua

--- Ezreal.lua
+++ Ezreal.lua
@@ -10,15 +10,13 @@ local function Initialize_menu()
     Menu.combo_mana_w = menu.slider_int( "Mana W", 0, 100, 10)
     Menu.combo_use_r = menu.checkbox("Use R", true)
     Menu.combo_mana_r = menu.slider_int( "Mana R", 0, 100, 10)
-
-    menu.label("Harass")
-    Menu.harass_use_q = menu.checkbox("Use Q", true)
-    Menu.harass_mana_q = menu.slider_int( "Mana Q", 0, 100, 40)
-    Menu.harass_use_w = menu.checkbox("Use W", true)
-    Menu.harass_mana_w = menu.slider_int( "Mana W", 0, 100, 40)
-    Menu.harass_use_r = menu.checkbox("Use R", false)
-    Menu.harass_mana_r = menu.slider_int( "Mana R", 0, 100, 40)
-
+   
+    menu.label("Mixed")
+    Menu.mixed_use_q = menu.checkbox("Use Q", true)
+    Menu.mixed_mana_q = menu.slider_int( "Mana Q", 0, 100, 10)
+    Menu.mixed_use_w = menu.checkbox("Use W", true)
+    Menu.mixed_mana_w = menu.slider_int( "Mana W", 0, 100, 10)
+            
     menu.label("Draw")
     Menu.draw_q = menu.checkbox("Draw Q range", true)
     Menu.draw_e = menu.checkbox("Draw E range", true)
@@ -26,13 +24,18 @@ end
 
 local function Init()
     Spell_limiter_q, Spell_limiter_w, Spell_limiter_r, max_mana  = 0,0,0,0
-
+    qSpeed = 2000
+    qRange = 1150
+    qWidth = 120
+    wSpeed = 1700
+    wRange = 1150
+    wWidth = 160
     Initialize_menu()
 end
 
 local function Use_Q(target)
     if Local_spellbook:get_spell_slot( spell_slot_t.q ):is_ready() and globals.get_game_time() > Spell_limiter_q then
-        local pred_pos = target:get_predicted_position( Local_hero:get_position() , 2000, 1200, 120, 0.25 )
+        local pred_pos = target:get_predicted_position( Local_hero:get_position() , qSpeed, qRange, qWidth, 0.25 )
         if pred_pos:length() > 1 and not collision.is_minion_in_line( pred_pos, 120 ) then
             input.send_spell( spell_slot_t.q , pred_pos )
             Spell_limiter_q = globals.get_game_time() + 0.5
@@ -42,7 +45,7 @@ end
 
 local function Use_W(target)
     if Local_spellbook:get_spell_slot( spell_slot_t.w ):is_ready() and globals.get_game_time() > Spell_limiter_w then
-        local pred_pos = target:get_predicted_position( Local_hero:get_position() , 1700, 1200, 160, 0.25 )
+        local pred_pos = target:get_predicted_position( Local_hero:get_position() , wSpeed, wRange, wWidth, 0.25 )
         if pred_pos:length() > 1 then
             input.send_spell( spell_slot_t.w, pred_pos )
             Spell_limiter_w = globals.get_game_time() + 0.5
@@ -66,18 +69,20 @@ local function Combo()
     local orbwalker_target = orbwalker.get_target()
     if orbwalker_target ~= -1 then
         local target = object_manager.get_by_index( orbwalker_target )
-        if Menu.combo_use_r:get_value() and Local_hero:get_mana() > max_mana * (Menu.combo_mana_r:get_value()/100) and Local_hero:get_mana() > 100 then Use_R(target) end
         if Menu.combo_use_w:get_value() and Local_hero:get_mana() > max_mana * (Menu.combo_mana_w:get_value()/100) and Local_hero:get_mana() > 50 then Use_W(target) end
         if Menu.combo_use_q:get_value() and Local_hero:get_mana() > max_mana * (Menu.combo_mana_q:get_value()/100) and Local_hero:get_mana() > 40 then Use_Q(target) end
     end
 end
 
-local function Harass()
-    local orbwalker_target = orbwalker.get_target()
-    if orbwalker_target ~= -1 then
-        local target = object_manager.get_by_index( orbwalker_target )
-        if Menu.harass_use_w:get_value() and Local_hero:get_mana() > max_mana * (Menu.harass_mana_w:get_value()/100) and Local_hero:get_mana() > 50 then Use_W(target) end
-        if Menu.harass_use_q:get_value() and Local_hero:get_mana() > max_mana * (Menu.harass_mana_q:get_value()/100) and Local_hero:get_mana() > 40 then Use_Q(target) end
+local function Mixed()
+    enemyTable = object_manager.get_valid_enemy_heroes()
+    for i,e in ipairs(enemyTable) do 
+        if (Local_hero:get_position() - e:get_position()):length() <= qRange then 
+            --render.text(vec2:new(200,200), tostring((Local_hero:get_position() - e:get_position()):length()),32,color:new( 255, 255, 255 ) ) end
+            local target = e
+            if Menu.mixed_use_q:get_value() and Local_hero:get_mana() > max_mana * (Menu.mixed_mana_q:get_value()/100) and Local_hero:get_mana() > 40 then Use_Q(target) end
+            if Menu.mixed_use_w:get_value() and Local_hero:get_mana() > max_mana * (Menu.mixed_mana_w:get_value()/100) and Local_hero:get_mana() > 40 then Use_W(target) end
+        end
     end
 end
 
@@ -93,7 +98,7 @@ end
 
 local function Tick()
     if input.is_key_down(67) then Combo() return end
-    if input.is_key_down(90) then Harass() return end
+    if input.is_key_down(84) then Mixed() return end
 end
 
 Init()
