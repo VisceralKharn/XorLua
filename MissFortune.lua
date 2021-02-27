7a16ddb46c38092d9148915b9a47d233c7c51010
Mark <>
2021-02-27 11:27:17 -0500

    Update MissFortune.lua

--- MissFortune.lua
+++ MissFortune.lua
@@ -38,8 +38,8 @@ local function Use_W(target)
     if Local_spellbook:get_spell_slot( spell_slot_t.w ):is_ready() and globals.get_game_time() > Spell_limiter_w then
         --pred speed, range, width, cast time
         local pred_pos = (target:get_position() - (Local_hero:get_position())):length()
-        if pred_pos:length() <= 550 then
-            input.send_spell( spell_slot_t.w , pred_pos )
+        if pred_pos <= 550 then
+            input.send_spell( spell_slot_t.w)
             Spell_limiter_e = globals.get_game_time() + 0.5
         end
     end
@@ -71,7 +71,8 @@ local function Combo()
     if orbwalker_target ~= -1 then
         local target = object_manager.get_by_index( orbwalker_target )
         if Menu.combo_use_q:get_value() and Local_hero:get_mana() > max_mana * (Menu.combo_mana_q:get_value()/100) and Local_hero:get_mana() > 40 then Use_Q(target) end
-      --  if Menu.combo_use_e:get_value() and Local_hero:get_mana() > max_mana * (Menu.combo_mana_e:get_value()/100) and Local_hero:get_mana() > 40 then Use_E() end
+        if Menu.combo_use_w:get_value() and Local_hero:get_mana() > max_mana * (Menu.combo_mana_w:get_value()/100) and Local_hero:get_mana() > 40 then Use_W(target) end
+        if Menu.combo_use_e:get_value() and Local_hero:get_mana() > max_mana * (Menu.combo_mana_e:get_value()/100) and Local_hero:get_mana() > 40 then Use_E(target) end
     end
 end
 
