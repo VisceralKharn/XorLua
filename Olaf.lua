52764781ef9c8653eafb3ae60788469e63fa9fc7
Mark <>
2021-03-11 14:09:24 -0500

    Update Olaf.lua

--- Olaf.lua
+++ Olaf.lua
@@ -4,12 +4,11 @@ local function Initialize_menu()
     menu.label("Olaf");
 
     menu.label("Combo")
-    Menu.combo_use_q = menu.checkbox("Use Q", true)
-    Menu.combo_mana_q = menu.slider_int( "Mana Q", 0, 100, 10)
+    Menu.combo_use_w = menu.checkbox("Use W", true)
+    Menu.combo_mana_w = menu.slider_int( "Mana W", 0, 100, 10)
+    Menu.combo_use_e = menu.checkbox("Use E", true)
+    Menu.combo_mana_e = menu.slider_int( "Mana E", 0, 100, 10)
 
-
-    menu.label("Draw")
-    Menu.draw_q = menu.checkbox("Draw Q range", true)
 
 end
 
@@ -19,37 +18,41 @@ local function Init()
     Initialize_menu()
 end
 
-local function Use_Q(target)
-    if Local_spellbook:get_spell_slot( spell_slot_t.q ):is_ready() and globals.get_game_time() > Spell_limiter_q then
+local function Draw()
+    Local_hero = object_manager.get_local()
+    Local_spellbook = Local_hero:get_spell_book()
+    if Local_hero:get_mana() > max_mana then max_mana = Local_hero:get_mana() return end
+end
+
+local function Use_W(target)
+    if Local_spellbook:get_spell_slot( spell_slot_t.w ):is_ready() and globals.get_game_time() > Spell_limiter_w then
+        local pred_pos = target:get_position()
         --pred speed, travel range, width, cast time
-        local pred_pos = target:get_predicted_position( Local_hero:get_position() , 2400, 790, 100, 0.25 )
-        if pred_pos:length() > 1 and not collision.is_minion_in_line( pred_pos, 130 ) then
-            input.send_spell( spell_slot_t.q , pred_pos )
-            Spell_limiter_q = globals.get_game_time() + 0.5
+            input.send_spell( spell_slot_t.w , pred_pos)
+            Spell_limiter_w = globals.get_game_time() + 0.5
+    end
+end
+
+local function Use_E(target)
+    if Local_spellbook:get_spell_slot( spell_slot_t.e ):is_ready() and globals.get_game_time() > Spell_limiter_e then
+        local pred_pos = target:get_position()
+        if pred_pos:length() > 1 then
+            input.send_spell( spell_slot_t.e , pred_pos )
+            Spell_limiter_e = globals.get_game_time() + 0.5
         end
     end
 end
 
-
-
-
 local function Combo()
     local orbwalker_target = orbwalker.get_target()
-    if orbwalker_target ~= -1 then
+    if orbwalker_target ~= nil then
         local target = object_manager.get_by_index( orbwalker_target )
-        if Menu.combo_use_q:get_value() and Local_hero:get_mana() > max_mana * (Menu.combo_mana_q:get_value()/100) and Local_hero:get_mana() > 40 then Use_Q(target) end
+        if Menu.combo_use_w:get_value() and Local_hero:get_mana() > max_mana * (Menu.combo_mana_w:get_value()/100) and Local_hero:get_mana() > 40 then Use_W(target) end
+        if Menu.combo_use_e:get_value() and Local_hero:get_mana() > max_mana * (Menu.combo_mana_e:get_value()/100) and Local_hero:get_mana() > 40 then Use_E(target) end
     end
 end
 
 
-local function Draw()
-    Local_hero = object_manager.get_local()
-    Local_spellbook = Local_hero:get_spell_book()
-
-    if Menu.draw_q:get_value() then render.circle_3d( Local_hero:get_position() , 1100, color:new( 0,125,255, 100 ) ) end
-
-    if Local_hero:get_mana() > max_mana then max_mana = Local_hero:get_mana() return end
-end
 
 local function Tick()
     if input.is_key_down(67) then Combo() return end
