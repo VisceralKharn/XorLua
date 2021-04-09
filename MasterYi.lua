6fb4eb01014571a23847257dcca5048fc0d15ffc
Mark <>
2021-04-09 19:37:18 -0400

    Update MasterYi.lua

--- MasterYi.lua
+++ MasterYi.lua
@@ -27,7 +27,7 @@ local function Use_Q(target)
     if Local_spellbook:get_spell_slot( spell_slot_t.q ):is_ready() and globals.get_game_time() > Spell_limiter_q then
         --pred speed, range, width, cast time
         local pred_pos = target:get_position()
-        if Local_hero:get_position() - pred_pos):length() <= qRange then
+        if (Local_hero:get_position() - pred_pos):length() <= qRange then
             input.send_spell( spell_slot_t.q , pred_pos )
             Spell_limiter_q = globals.get_game_time() + 0.5
         end
