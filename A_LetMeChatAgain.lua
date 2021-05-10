e4418dceb325b24f291e779025aa5b86b2934e13
Mark <>
2021-05-10 19:05:48 -0400

    Update A_LetMeChatAgain.lua

--- A_LetMeChatAgain.lua
+++ A_LetMeChatAgain.lua
@@ -16,12 +16,14 @@ local function Tick()
         config.set_value('orbwalker_last_hit_keybind',35)
         config.set_value('orbwalker_lane_clear_keybind',35)
         chatOn = 1
+        return chatOn
     end
     if input.is_key_down(13) and chatOn == 1 then
         config.set_value('orbwalker_keybind',32)
         config.set_value('orbwalker_last_hit_keybind',84)
         config.set_value('orbwalker_lane_clear_keybind',86)
-        chatOn = 0    
+        chatOn = 0
+        return chatOn    
     end
     if input.is_key_down(35) then 
         print(config.dump_vars()) 
